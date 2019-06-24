a0636db0a159ce2b21396dc09d77ec53c850c8d9
Leo Eduardo da Silva <leo.eduardo_silva@hotmail.com>
2019-06-24 19:37:14 -0300

    added search screen

--- src/main/java/com/gym/fe/UIInit.java
+++ src/main/java/com/gym/fe/UIInit.java
@@ -1,47 +1,197 @@
 package com.gym.fe;
 
+import com.gym.be.controller.PaymentController;
+import com.gym.be.controller.RegisterController;
+import com.gym.be.payment.PaymentModel;
 import lombok.var;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.awt.event.ActionEvent;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import javax.swing.*;
 
-import javax.swing.GroupLayout;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JFrame;
-
+@Service
 public class UIInit extends JFrame {
-  public void initUI() {
+    @Autowired
+    private RegisterController registerController;
+    @Autowired
+    private PaymentController paymentController;
 
-    var quitButton = new JButton("Quit");
+    public void initUI() {
 
-    quitButton.addActionListener((ActionEvent event) -> {
-      System.exit(0);
-    });
+        this.setLayout(null);
 
-    //Put UI class here
-    createLayout(quitButton);
+        //Criação do label Nome
+        final JLabel clienteLabel = new JLabel("Cliente:");
+        createLayout(clienteLabel, this, 150, 50, 5, 5);
 
-    setTitle("Quit button");
-    setSize(300, 200);
-    setLocationRelativeTo(null);
-    setDefaultCloseOperation(EXIT_ON_CLOSE);
-  }
+        //Criação do TextField Nome
+        final JTextField clienteText = new JTextField();
+        createLayout(clienteText, this, 100, 20, 5, 40);
 
-  private void createLayout(JComponent... arg) {
+        //Criação do label Data Inicial
+        final JLabel dtInicialLabel = new JLabel("Data Inicial:");
+        createLayout(dtInicialLabel, this, 150, 50, 5, 45);
 
-    var pane = getContentPane();
-    var gl = new GroupLayout(pane);
-    pane.setLayout(gl);
+        //Criação do DateTextField Data Inicial
+        final JTextField dtInicialTextField = new JTextField();
+        dtInicialTextField.setText(new SimpleDateFormat("dd/MM/yyyy").format(new Date(System.currentTimeMillis())));
+        createLayout(dtInicialTextField, this, 100, 20, 5, 80);
 
-    gl.setAutoCreateContainerGaps(true);
+        //Criação do label Data Final
+        final JLabel dtFinalLabel = new JLabel("Data Final:");
+        createLayout(dtFinalLabel, this, 150, 50, 170, 45);
 
-    gl.setHorizontalGroup(gl.createSequentialGroup()
-        .addComponent(arg[0])
-    );
+        //Criação do DateTextField Data Final
+        final JTextField dtFinalTextField = new JTextField();
+        dtFinalTextField.setText(new SimpleDateFormat("dd/MM/yyyy").format(new Date(System.currentTimeMillis())));
+        createLayout(dtFinalTextField, this, 100, 20, 165, 80);
 
-    gl.setVerticalGroup(gl.createSequentialGroup()
-        .addComponent(arg[0])
-    );
-  }
+        //Criação do label CheckBox
+        final JLabel statusLabel = new JLabel("Status:");
+        createLayout(statusLabel, this, 150, 50, 5, 85);
 
+        //Criação do CheckBox Pago
+        final JCheckBox pagoCheckBox = new JCheckBox("Pago");
+        createLayout(pagoCheckBox, this, 150, 50, 5, 105);
+
+        //Criação do label Modalidades
+        final JLabel modalidadeLabel = new JLabel("Modalidade:");
+        createLayout(modalidadeLabel, this, 150, 50, 170, 5);
+
+        //Criação do TextField Modalidade
+        final JTextField modalidadeText = new JTextField();
+        createLayout(modalidadeText, this, 100, 20, 165, 40);
+
+        //Dados que serão exibidos na tabelaLimpa
+        String[][] dadosLimpo = {
+                {"", "", "", "", ""}
+        };
+
+        //Colunas da tabela
+        String[] colunasTabela = {"Cliente", "Modalidades", "Data Vencimento", "Valor", "Pago"};
+
+        //Criação do JTabel TabelaLimpa
+        JTable tabelaLimpa;
+
+        //Inicialização da tabelaLimpa com os dados e coluna
+        tabelaLimpa = new JTable(dadosLimpo, colunasTabela);
+
+        //Criação do JScollPane painelTabelaLimpa
+        JScrollPane painelTabelaLimpa = new JScrollPane(tabelaLimpa);
+
+        createLayout(painelTabelaLimpa, this, 300, 200, 5, 160);
+
+        //Frame para o popup do botão Pesquisar
+        final JFrame pesquisaFrame = new JFrame("Frame de Pesquisa");
+        pesquisaFrame.setSize(250, 150);
+
+        //Criação do button Pesquisar
+        var pesquisarButton = new JButton("Pesquisar");
+
+        //Ação do botão pesquisar
+        pesquisarButton.addActionListener((ActionEvent event) -> {
+
+            ////////////////// Atualiza JTabel Tabela //////////////////
+
+            //Passa a data inicial já separada em ano, mês e dia para vecDtInicial
+            String[] vecDtInicial = dtInicialTextField.getText().split("/");
+
+            //Passa vecDtInicial para listDtInicial
+            List<String> listDtInicial = new ArrayList<>();
+            listDtInicial.addAll(Arrays.asList(vecDtInicial));
+
+            //Passa para dtInicial o listDtInicial
+            Date dtInicial = new Date(Integer.parseInt(listDtInicial.get(2)), Integer.parseInt(listDtInicial.get(1)), Integer.parseInt(listDtInicial.get(0)));
+
+            //Passa a data final já separada em ano, mês e dia para vecDtFinal
+            String[] vecDtFinal = dtFinalTextField.getText().split("/");
+
+            //Passa vecDtFinal para listDtFinal
+            List<String> listDtFinal = new ArrayList<>();
+            listDtFinal.addAll(Arrays.asList(vecDtFinal));
+
+            //Passa para dtFinal o listDtFinal
+            Date dtFinal = new Date(Integer.parseInt(listDtFinal.get(2)), Integer.parseInt(listDtFinal.get(1)), Integer.parseInt(listDtFinal.get(0)));
+
+            //Passa as modalidade já formatadas para vecModalidadeText
+            String[] vecModalidadeText = modalidadeText.getText().replaceAll(" ", "").split(",");
+
+            //Passa vecModalidadeText para listModalidades
+            List<String> listModalidade = new ArrayList<>();
+            listModalidade.addAll(Arrays.asList(vecModalidadeText));
+
+            List<PaymentModel> listRetorno = (List<PaymentModel>) paymentController.paymentGet("Gabriel", null, null, null, null).getBody();
+            //paymentController.paymentGet(clienteText.getText(),dtInicial,dtFinal,pagoCheckBox.isSelected(),listModalidade).getBody();
+
+            //Matriz usada na criação tabela
+            String[][] dados = new String[listRetorno.size()][5];
+
+            //Popula a matriz com os dados
+            listRetorno.forEach(payment -> {
+                int i = listRetorno.indexOf(payment);
+                String nome = payment.getRegister().getName();
+                dados[i][0] = nome;
+                String modalidades = payment.getRegister().getModalities().toString().replace("]", "");
+                dados[i][1] = modalidades.replace("[", "");
+                SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
+                String vencimento = fmt.format(payment.getPaymentDate());
+                dados[i][2] = vencimento;
+                String valor = payment.getRegister().getValue().toString();
+                dados[i][3] = valor;
+                String pago = payment.getPayed() ? "Pago" : "Pendente";
+                dados[i][4] = pago;
+            });
+
+            //Criação do JTabel Tabela
+            JTable tabela;
+
+            //Inicialização da tabela com os dados e coluna
+            tabela = new JTable(dados, colunasTabela);
+
+            //Desabilita a tabelaLimpa e o painelTabelaLimpa
+            tabelaLimpa.setVisible(false);
+            painelTabelaLimpa.setVisible(false);
+
+            //Criação do JScollPane painelTabela
+            JScrollPane painelTabela = new JScrollPane(tabela);
+
+            createLayout(painelTabela, this, 300, 200, 5, 160);
+        });
+
+        createLayout(pesquisarButton, this, 100, 30, 162, 117);
+
+        ////////////////// CADASTRO //////////////////
+
+        //Frame para o popup do botão Cadastro
+        final JFrame cadastroFrame = new JFrame("Cadastro");
+        cadastroFrame.setSize(250, 150);
+
+        //Criação do button Cadastro
+        var cadastrarButton = new JButton("Cadastrar");
+
+        cadastrarButton.addActionListener((ActionEvent event) -> {
+            JOptionPane.showMessageDialog(cadastroFrame, "Cadastro");
+        });
+
+        createLayout(cadastrarButton, this, 100, 30, 110, 400);
+
+        setTitle("Gym Application");
+        setSize(310, 450);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    void createLayout(JComponent componente, JFrame frame, int tamX, int tamY, int locX, int locY) {
+        //Tamanho
+        componente.setSize(tamX, tamY);
+
+        //Posição
+        componente.setLocation(locX, locY);
+
+        //Adiciona componente ao Frame
+        frame.add(componente);
+    }
 }
