ac3695bc1f89a484909e7171e3e1a5796427686b
gsimionato-daitan <gsimionato@daitangroup.com>
2019-06-25 22:49:10 -0300

    Dialog for registering

--- src/main/java/com/gym/fe/UIInit.java
+++ src/main/java/com/gym/fe/UIInit.java
@@ -219,9 +219,56 @@ public class UIInit extends JFrame {
     //Criação do button Cadastro
     var cadastrarButton = new JButton("Cadastrar");
 
+    JLabel nameLabel = new JLabel("Nome do Cliente: ");
+//        createLayout(nameLabel, cadastroFrame, 150, 20, 5, 5);
+//
+    JTextField nameField = new JTextField(20);
+//        createLayout(nameField, cadastroFrame, 100, 20, 5, 65);
+
+    JLabel dateLabel = new JLabel("Data de Vencimento: ");
+//        createLayout(dateLabel, cadastroFrame, 150, 50, 5, 5);
+//
+    JTextField paymentField = new JTextField();
+//        createLayout(paymentField, cadastroFrame, 150, 50, 5, 45);
+
+    JLabel valueLabel = new JLabel("Mensalidade: ");
+
+    JTextField valueField = new JTextField();
+
+    JLabel modalitiesLabel = new JLabel("Modalidades (separar por vírgula):");
+    JTextField modalitiesField = new JTextField();
+
+    JButton confirmButton = new JButton("Cadastrar");
+    confirmButton.setEnabled(false);
+
+    final JComponent[] inputs = new JComponent[] {
+            nameLabel,
+            nameField,
+            dateLabel,
+            paymentField,
+            valueLabel,
+            valueField,
+            modalitiesLabel,
+            modalitiesField,
+            confirmButton,
+    };
+
     cadastrarButton.addActionListener((ActionEvent event) -> {
-      JOptionPane.showMessageDialog(cadastroFrame, "Cadastro");
+      JOptionPane.showOptionDialog(
+              null,
+              inputs,
+              "Cadastro",
+              JOptionPane.DEFAULT_OPTION,
+              JOptionPane.INFORMATION_MESSAGE,
+              null,
+              new Object[]{confirmButton},
+              confirmButton
+      );
     });
+
+//    cadastrarButton.addActionListener((ActionEvent event) -> {
+//      JOptionPane.showMessageDialog(cadastroFrame, "Cadastro");
+//    });
 
     createLayout(cadastrarButton, this, 100, 30, 110, 400);
 
